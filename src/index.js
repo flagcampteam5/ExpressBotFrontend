e7d137899052174f547413995982632e8992883d
Zhongyu Li <mrcarlos9500@gmail.com>
2020-08-10 23:53:03 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-    <React.StrictMode >
+    <React.StrictMode>
         <p>Hong Lu made a change</p>
         <p>Zihao Wang made a change</p>
         <p>Zhongyu Li made a change</p>
