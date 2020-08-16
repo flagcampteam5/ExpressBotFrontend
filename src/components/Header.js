337071ec801b963bab15fba4aea05f50b88d6270
Danny Wang <wang_zihao@Danny-WangdeMacBook-Pro.local>
2020-08-15 22:50:54 -0700

    first try

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,28 @@
-import React, {Component} from 'react';
-import { Link} from 'react-router-dom';
-import { Menu } from 'antd';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
 
 class Header extends Component {
     render() {
         return (
             <header className="App-header">
-                <div className="logo"/>
-                <Menu className = "Menu" mode = "horizontal" style = {{float: ''}}>
-                    
-                    <Menu.Item key = "About Us" href='./AboutUs'>
-                        <Link to={'/aboutus'}>About Us</Link>
+                <div className="logo" />
+                <Menu className="Menu" mode="horizontal" style={{ float: "" }}>
+                    <Menu.Item
+                        key="About Us"
+                        href="./AboutUs"
+                        className="aboutus"
+                    >
+                        <Link to={"/aboutus"}>About Us</Link>
                     </Menu.Item>
-                    <Menu.Item key = "Service">
-                        <Link to={'/'}>Service</Link>
+                    <Menu.Item key="Service" className="service">
+                        <Link to={"/"}>Service</Link>
                     </Menu.Item>
-                    <Menu.Item key = "Track"> 
-                        <Link to={'/tracking'}>Track</Link>
+                    <Menu.Item key="Track" className="track">
+                        <Link to={"/tracking"}>Track</Link>
                     </Menu.Item>
                 </Menu>
-                
             </header>
-                 
-            
-            
         );
     }
 }
--- src/components/webpages/Service.js
+++ src/components/webpages/Service.js
@@ -1,51 +1,63 @@
-import React, {Component} from 'react';
-import { Redirect, Link} from 'react-router-dom';
-import { Card, Input, Form, Button } from 'antd';
+import React, { Component } from "react";
+import { Redirect, Link } from "react-router-dom";
+import { Card, Input, Form, Button } from "antd";
 
-
-
-class Service extends Component{
-    
-    constructor(){
+class Service extends Component {
+    constructor() {
         super();
         this.state = {
-            redirect : false
-        }
+            redirect: false,
+        };
     }
     setRedirect = () => {
         this.setState({
-            redirect: true
-        })
-    }
+            redirect: true,
+        });
+    };
     renderRedirect = () => {
-        if(this.state.redirect){
-            return <Redirect to='/request'/>
+        if (this.state.redirect) {
+            return <Redirect to="/request" />;
         }
-    }
-    render(){
-        return(
-           <div className="service">
-                <div className="Block" > 
-                    <p> This is the place where we put a short introduction to our service</p>
-                    <img></img>
+    };
+    render() {
+        return (
+            <div className="service">
+                <div className="Block">
+                    <div className="introduction">
+                        <span>Introduction to our web</span>
+                    </div>
+                    <div className="picture">
+                        <img src="../../asset/images/RobotDelivery.jpeg"></img>
+                    </div>
                 </div>
-               <Card title="Request A Pick-Up">
-                   <Form>
-                       <Form.Item>
-                            <Input placeholder="Enter Your Pick-Up Location"/>
-                       </Form.Item>
-                       <Form.Item>
-                           <Input placeholder="Enter Your Destination"/>
-                       </Form.Item>
-                       <Form.Item>
-                           <Button type="primary" >
-                               <Link to={'/request'}>Request</Link>
+                <div className="form-div">
+                    <Form className="form">
+                        <Form.Item>
+                            <div>
+                                <span className="span">Request pick-up</span>
+                            </div>
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                className="input"
+                                placeholder="Enter Your Pick-Up Location"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                                className="input"
+                                placeholder="Enter Your Destination"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button className="button">
+                                <Link to={"/request"}>Request</Link>
                             </Button>
-                       </Form.Item>
-                   </Form>
-               </Card>
-           </div>
-        )
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+        );
     }
 }
 export default Service;
