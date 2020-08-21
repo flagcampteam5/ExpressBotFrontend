bb6c2b56605e4ae45ba07377a98896e574b19376
flagcampteam5 <69276519+flagcampteam5@users.noreply.github.com>
2020-08-21 12:09:28 -0700

    Delete constants.js

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_KEY = "AIzaSyDlJwq7vkMTDJZdiL_faErvEgozJJ4TCQ4";
-
-export const GOOGLE_PLACES_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_KEY}&libraries=places`;
